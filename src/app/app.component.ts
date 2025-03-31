91c7ea2a1afb8d4b2c5588d4baf2868bdc7fb66b
gaburoff <gabrielcha@uroff.net>
2025-03-31 20:45:21 -0300

    use local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,7 +45,7 @@ export class AppComponent implements OnInit {
       const vision = await FilesetResolver.forVisionTasks('/wasm');
       const handLandmarker = await HandLandmarker.createFromOptions(vision, {
         baseOptions: {
-          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+          modelAssetPath: `/hand_landmarker.task`,
           delegate: 'GPU',
         },
         runningMode: 'VIDEO',
